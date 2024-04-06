e65f930ef3e172eb07e16034b800b73b8ba73eed
unknown <kitsdk@163.com>
2024-04-06 16:14:16 +0800

    增加转换参数，用于表示再转为img之后是否继续转为pdf

--- pdfviewer/include/pdf2image/pdf2ImageThreadSingle.h
+++ pdfviewer/include/pdf2image/pdf2ImageThreadSingle.h
@@ -19,12 +19,13 @@ class pdf2imageThreadSingle : public QObject, public QRunnable {
   // explicit  pdf2imageThreadSingle(QObject *parent = nullptr) :
   // QObject(parent), m_value(0) {}
   explicit pdf2imageThreadSingle(QObject* parent = nullptr);
-  // pdf2imageThreadSingle();
   void setSourceFile(QString sourceFile);
   void setImagePath(QString imagePath);
   void setTargetFile(QString targetFile);
   int pdf2image(string pdfFile, string imagePath, int resolution);
   void setResolution(int resolution);
+  //设置是否启用图片转pdf功能
+  void setIs2pdf(bool is2pdf);
   void run() override;
   QMutex m_mutex;
   wMap* m_wmap;
@@ -37,7 +38,7 @@ class pdf2imageThreadSingle : public QObject, public QRunnable {
 
  private:
   int m_id, m_resolution;
-  ;
+  bool m_is2pdf = true;
   QString m_sourceFile, m_imagePath, m_targetFile;
 };
 
